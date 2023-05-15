c5f7503208c05c8d78f9b08d31000d5d0afc1ebb
Kenny Wan <kennethzwan@gmail.com>
2023-05-14 17:54:37 -0700

    created firebase & initialized it, add new folders

---
+++ backend/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA7N3HNdokIkp8uHhbGilzdZcSqNc7KGSo",
+    authDomain: "bryft-382e7.firebaseapp.com",
+    projectId: "bryft-382e7",
+    storageBucket: "bryft-382e7.appspot.com",
+    messagingSenderId: "16895573382",
+    appId: "1:16895573382:web:c426996e8f867f49a78007",
+    measurementId: "G-XK6V3NESWR"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+
+  export {
+    app, db
+  };
