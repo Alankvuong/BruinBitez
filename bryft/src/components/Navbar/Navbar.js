a057fc727d591606c2a966a8b9255254814372c2
Alan Vuong <alankvuong@gmail.com>
2023-05-18 02:13:08 -0700

    Created Navbar component | Added navbar to homepage component | Added react routes in App.js file when navbar is clicked on

---
+++ bryft/src/components/Navbar/Navbar.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+function Navbar() {
+
+    return (
+        <nav className="navbar">
+            <div className="navbar-logo">
+                <Link to="/">BRYFT</Link>
+            </div>
+                <ul className="navbar-links">
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/rides">Rides</Link>
+                    </li>
+                    <li>
+                        <Link to="/profile">Profile</Link>
+                    </li>
+                </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
