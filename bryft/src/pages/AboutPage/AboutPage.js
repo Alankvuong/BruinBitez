691d1297cccf6f0c50700a793b33ce45d2e07dd6
Sophia Lee <sophiaylee23@gmail.com>
2023-06-03 17:28:34 -0700

    adding styling to about page

--- bryft/src/pages/AboutPage/AboutPage.js
+++ bryft/src/pages/AboutPage/AboutPage.js
@@ -6,22 +6,23 @@ import Instagram from "@mui/icons-material/Instagram";
 import LinkedIn from "@mui/icons-material/LinkedIn";
 import { Link } from 'react-router-dom';
 
+
 function AboutPage() {
     return (
         <div>
         <Navbar />
-        <div>
-            <h1>about us</h1>
-            {/* <img/> */}
-            <p>Welcome to Bryft, a rideshare platform exclusively designed by college students, for college students. We understand the challenges of commuting to and from campus, and our mission is to provide a safe, convenient, and reliable solution tailored specifically to meet the needs of today's students. Whether you’re a driver searching for some travel buddies and some extra cash, or a rider looking to connect with fellow students and hitch a cheap ride, Bryft is the perfect platform for you. 
+        <div className="brick" style = {{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                <h1 className="title">about us</h1>
+                <p className="text">Welcome to Bryft, a rideshare platform exclusively designed by college students, for college students. We understand the challenges of commuting to and from campus, and our mission is to provide a safe, convenient, and reliable solution tailored specifically to meet the needs of today's students. Whether you’re a driver searching for some travel buddies and some extra cash, or a rider looking to connect with fellow students and hitch a cheap ride, Bryft is the perfect platform for you. 
                 <br/>
                 <br/>
-                At Bryft, we strive to create a community where students can easily connect with fellow classmates, find affordable rides, and forge meaningful connections along the way. Join Bryft today and revolutionize the way you travel to and from campus. Together, let's navigate the roads of education, connection, and opportunity.
-            </p>
-        </div>
-        <div>
-            <h2>connect with us</h2>
-            <div>
+                At Bryft, we strive to create a community where students can easily connect with fellow classmates, find affordable rides, and forge meaningful connections along the way. Join Bryft today and revolutionize the way you travel to and from campus. Together, let's navigate the roads of education, connection, and opportunity.</p>
+            </div>
+            <div className="brick">
+                <h2 className="subtitle">
+                    connect with us
+                </h2>
+                <div style={{ display: 'flex', justifyContent: 'center'}}>
                 <IconButton button component={Link} to="youtube.com">
                     <Email fontSize = "large"/>
                 </IconButton>
@@ -31,10 +32,9 @@ function AboutPage() {
                 <IconButton button component={Link} to="youtube.com">
                     <LinkedIn fontSize = "large"/>
                 </IconButton>
+                </div>
             </div>
-        </div>
         </div>
     )
 }
 export default AboutPage;
-
