720919a930de445d39f7222480f9b050077456db
Kenny Wan <kennethzwan@gmail.com>
2023-06-03 22:33:55 -0700

    added firebase auth, google sign in, sign in, logout button, popup for login, state saving, userprofile page creation

--- bryft/src/components/Navbar/Navbar.js
+++ bryft/src/components/Navbar/Navbar.js
@@ -1,8 +1,40 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+// import { Popup } from "reactjs-popup";
+import Modal from 'react-modal';
+import { Link, Navigate } from "react-router-dom";
+import {UserAuth} from '../../context/AuthContext'
 import "./Navbar.css";
 
+
 function Navbar() {
+    const {user, logOut} = UserAuth();
+    const { googleSignIn } = UserAuth();
+    
+    const handleSignIn = async () => {
+        try {
+            await googleSignIn();            
+        } catch(error) {
+            console.log(error);
+        }
+    };
+
+    const handleSignOut = async () => {
+        try {
+            await logOut()
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    function openModal() {
+        setIsOpen(true);
+      }
+
+    function closeModal() {
+    setIsOpen(false);
+    }
+
+    const [modalIsOpen, setIsOpen] = React.useState(false);
 
     return (
         <nav className="navbar">
@@ -21,6 +53,45 @@ function Navbar() {
                     </li>
                     <li>
                         <Link to="/user-profile">Profile</Link>
+                    </li>
+                    <li>
+                        {user ? (
+                            <button onClick={handleSignOut}>Logout</button>
+                        ) : ( 
+                        <div>
+                            <button onClick={openModal}>Open Modal</button>
+                            <div className="modalContainer">
+                                <Modal
+                                    isOpen={modalIsOpen}
+                                    // onAfterOpen={afterOpenModal}
+                                    onRequestClose={closeModal}
+                                    style={{
+                                        overlay: {
+                                            // position: 'fixed',
+                                            width: '100%',
+                                            height: '100%',
+                                            alignSelf: 'center',
+                                            backgroundColor: 'rgba(0, 0, 0, 0.6)',
+                                        },
+                                        content: {
+                                            width: '30%',
+                                            height: '75%',
+                                            borderRadius: 34,
+                                            boxShadow: 'rgba(0, 0, 0, 0.45) 0px 2px 10px',
+                                            position: 'absolute',
+                                            top: '12%',
+                                            left: '34%',
+                                            marginTop: '-1%',
+                                        }
+                                    }}
+                                    contentLabel="Login Modal"
+                                >
+                                <button onClick={handleSignIn}>Sign In</button>
+                                <div>I am a modal</div>
+                                </Modal>
+                            </div>
+                        </div>
+                    )}
                     </li>
                 </ul>
         </nav>
