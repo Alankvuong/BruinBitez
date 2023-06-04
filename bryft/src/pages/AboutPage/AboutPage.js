1d2523586137db231b06ed17a348fd82efa3c73d
Sophia Lee <sophiaylee23@gmail.com>
2023-06-03 19:24:45 -0700

    add in images

--- bryft/src/pages/AboutPage/AboutPage.js
+++ bryft/src/pages/AboutPage/AboutPage.js
@@ -6,6 +6,12 @@ import Instagram from "@mui/icons-material/Instagram";
 import LinkedIn from "@mui/icons-material/LinkedIn";
 import { Link } from 'react-router-dom';
 
+import alan from '/Users/sophialee/Desktop/for git/Bryft/bryft/src/components/alanheadshot.jpeg';
+import kenny from '/Users/sophialee/Desktop/for git/Bryft/bryft/src/components/kennyheadshot.jpg';
+import kevin from '/Users/sophialee/Desktop/for git/Bryft/bryft/src/components/kevinheadshot.jpeg';
+import mikey from '/Users/sophialee/Desktop/for git/Bryft/bryft/src/components/mikeyheadshot.jpeg';
+import sophia from '/Users/sophialee/Desktop/for git/Bryft/bryft/src/components/sophiaheadshot.jpg';
+
 
 function AboutPage() {
     return (
@@ -13,6 +19,18 @@ function AboutPage() {
         <Navbar />
         <div className="brick" style = {{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
                 <h1 className="title">about us</h1>
+                <div className='flex-row'>
+                    <img src ={alan} style={{ width: '20%', maxWidth: '200px', margin: '0 10px' }}/>
+                    <div style={{ width: '15px' }}></div> {/* spacing */}
+                    <img src ={kenny} style={{ width: '20%', maxWidth: '200px', margin: '0 10px' }}/>
+                    <div style={{ width: '15px' }}></div> {/* spacing */}
+                    <img src ={kevin} style={{ width: '20%', maxWidth: '200px', margin: '0 10px' }}/>
+                    <div style={{ width: '15px' }}></div> {/* spacing */}
+                    <img src ={mikey} style={{ width: '20%', maxWidth: '200px', margin: '0 10px' }}/>
+                    <div style={{ width: '15px' }}></div> {/* spacing */}
+                    <img src ={sophia} style={{ width: '20%', maxWidth: '200px', margin: '0 10px' }}/>
+                </div>
+                <br/>
                 <p className="text">Welcome to Bryft, a rideshare platform exclusively designed by college students, for college students. We understand the challenges of commuting to and from campus, and our mission is to provide a safe, convenient, and reliable solution tailored specifically to meet the needs of today's students. Whether you’re a driver searching for some travel buddies and some extra cash, or a rider looking to connect with fellow students and hitch a cheap ride, Bryft is the perfect platform for you. 
                 <br/>
                 <br/>
