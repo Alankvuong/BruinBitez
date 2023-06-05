da8fbd56b046c301679d115448f4f194725b8054
mikeychoi <131425709+mikeychoi@users.noreply.github.com>
2023-06-05 05:09:39 -0700

    Clean up HomePage.js

--- bryft/src/pages/HomePage/HomePage.js
+++ bryft/src/pages/HomePage/HomePage.js
@@ -1,34 +1,46 @@
 import "./HomePage.css";
 import React from 'react';
 import Navbar from "../../components/Navbar/Navbar";
-import backdrop from './royce-backdrop.png';
-import icon1 from './icon1.png';
-import icon2 from './icon2.png';
-import icon3 from './icon3.png';
+import backdrop from './backdrop-royce.png';
+import icon1 from './icon-computer.png';
+import icon2 from './icon-students.png';
+import icon3 from './icon-money.png';
 import { Link } from "react-router-dom";
 
 function HomePage() {
     return (
         <React.Fragment>
+            <main>
             <Navbar />
             <img className="backdrop" src={backdrop} alt="Backdrop" />
-            <h1>BETTER TRANSPORT FOR BRUINS.</h1>
-            <Link to="/rides"><div className="textbox t1">Create a group</div></Link>
-            <Link to="/rides"><div className="textbox t2">Find a ride</div></Link>
-            <div className="backing"></div>
-            <img className="icon i1" src={icon1} alt="icon1" />
+                <h1>BETTER TRANSPORT FOR BRUINS.</h1>
+                
+            <div className="quickcontainer">
+                <Link to="/rides"><div className="textbox color1">Create a group</div></Link>
+                <Link to="/rides"><div className="textbox color2">Find a ride</div></Link>
+            </div>
             
-            <div className="divider d1"></div>
-            <img className="icon i2" src={icon2} alt="icon2" />
-            <div className="divider d2"></div>
-            <img className="icon i3" src={icon3} alt="icon3" />
-
-            <h2 className="sh1">Easy to use</h2>
-            <h2 className="sh2">Ride with fellow Bruins</h2>
-            <h2 className="sh3">Save money</h2>
-            <p style={{ left: "5%" }}>Browse existing ridesharing groups or create your own for an easy commute to and from UCLA</p>
-            <p style={{ left: "37%" }}>Groups are exclusively available to current UCLA students, eliminating the uncertainty of carpooling with complete strangers</p>
-            <p style={{ left: "70%" }}>Cut down on expensive rides by splitting the fare among several people</p>
+            <div className="infocontainer">
+            
+                <div className="infobox">
+                    <img className="icon" src={icon1} alt="icon1" />
+                    <h2>Easy to use</h2>
+                    <p>Browse existing ridesharing groups or create your own for an easy commute to and from UCLA</p>
+                </div>
+                <div className="divider"></div>    
+                <div className="infobox">
+                    <img className="icon" src={icon2} alt="icon2" />
+                    <h2>Ride with fellow Bruins</h2>
+                    <p>Groups are exclusively available to current UCLA students, eliminating the uncertainty of carpooling with complete strangers</p>
+                </div>
+                <div className="divider"></div>
+                <div className="infobox">
+                    <img className="icon" src={icon3} alt="icon3" />
+                    <h2>Save money</h2>
+                    <p>Cut down on expensive rides by splitting the fare among several people</p>
+                </div>
+            </div>
+            </main>
         </React.Fragment>
     )
 }
