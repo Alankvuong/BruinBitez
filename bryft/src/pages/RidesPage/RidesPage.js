001dbbd4686b38c8f42b7f11b894c686b07467ee
Kevin Yuan <kyuan191787@gmail.com>
2023-06-09 02:19:25 -0700

    - placeholder for price & store price as double - sort rides by closest date to furthest date

--- bryft/src/pages/RidesPage/RidesPage.js
+++ bryft/src/pages/RidesPage/RidesPage.js
@@ -20,23 +20,30 @@ export default function RidesPage() {
         console.log(queryParams);
         axios.get('http://localhost:8000/api/get-rides', { params: queryParams })
             .then((response) => {
-                setFetchedRides(response.data);
-                console.log("fetched rides", response.data);
+                // Sort rides from closest date to farthest date
+                const sortRides = response.data.sort((a, b) => {
+                    const dateTimeA = new Date(a.dateTime);
+                    const dateTimeB = new Date(b.dateTime);
+                    return dateTimeA - dateTimeB;
+                });
+
+                setFetchedRides(sortRides);
             })
             .catch((error) => {
                 console.error('Error:', error);
-            })
+            });
     }, [queryParams]);
 
     const handleQueryParamChange = (e) => {
 
-        //deselect previous checkbox
         if (selectedValues[e.target.name] === e.target.value) {
+            //deselect previous or currently selected checkbox
             setQueryParams({ ...queryParams, [e.target.name]: '' });
-            setSelectedValues({...selectedValues, [e.target.name]: ''});
+            setSelectedValues({ ...selectedValues, [e.target.name]: '' });
         } else {
+            //select new checkbox
             setQueryParams({ ...queryParams, [e.target.name]: e.target.value });
-            setSelectedValues({...selectedValues, [e.target.name]: e.target.value});
+            setSelectedValues({ ...selectedValues, [e.target.name]: e.target.value });
         }
     }
 
@@ -48,44 +55,28 @@ export default function RidesPage() {
                     Where are you traveling to?
                 </Typography>
                 <Box display="flex" marginLeft="auto" mb={2} >
-                    {/* <Box>
-                    <TextField 
-                        label="From"
-                        name="origin"
-                        size="small"
-                        value={queryParams.origin || ''}
-                        onChange={handleQueryParamChange} 
-                    />
-                    <TextField 
-                        label="To"
-                        name="destination"
-                        size="small"
-                        value={queryParams.destination || ''}
-                        onChange={handleQueryParamChange} 
-                    />
-                    </Box> */}
                     <RideModal />
                 </Box>
                 <Box display="flex" flexDirection="row" width="100%">
                     <Container className="filter-container">
-                        
-                            <span className="filter-category">Origin</span>
-                            <FormControlLabel control={<Checkbox checked={selectedValues.origin === 'UCLA'} onChange={handleQueryParamChange}/>} label="UCLA" value="UCLA" className="filter" name="origin"/>
-                            <FormControlLabel control={<Checkbox checked={selectedValues.origin === 'LAX'} onChange={handleQueryParamChange}/>} label="LAX" value="LAX" className="filter" name="origin"/>
-                            <FormControlLabel control={<Checkbox checked={selectedValues.origin === 'USC'} onChange={handleQueryParamChange}/>} label="USC" value="USC" className="filter" name="origin"/>
-                            <FormControlLabel control={<Checkbox checked={selectedValues.origin === 'San Diego'} onChange={handleQueryParamChange}/>} label="San Diego" value="San Diego" className="filter" name="origin"/>
-                            <FormControlLabel control={<Checkbox checked={selectedValues.origin === 'Koreatown'} onChange={handleQueryParamChange}/>} label="Koreatown" value="Koreatown" className="filter" name="origin"/>
-                            <FormControlLabel control={<Checkbox checked={selectedValues.origin === 'Irvine'} onChange={handleQueryParamChange}/>} label="Irvine" value="Irvine" className="filter" name="origin"/>
-                            <FormControlLabel control={<Checkbox checked={selectedValues.origin === 'Orange County'} onChange={handleQueryParamChange}/>} label="Orange County" value="Orange County" className="filter" name="origin"/>
 
-                            <span className="filter-category">Destination</span>
-                            <FormControlLabel control={<Checkbox checked={selectedValues.destination === 'UCLA'} onChange={handleQueryParamChange}/>} label="UCLA" value="UCLA" className="filter" name="destination"/>
-                            <FormControlLabel control={<Checkbox checked={selectedValues.destination === 'LAX'} onChange={handleQueryParamChange}/>} label="LAX" value="LAX" className="filter" name="destination"/>
-                            <FormControlLabel control={<Checkbox checked={selectedValues.destination === 'USC'} onChange={handleQueryParamChange}/>} label="USC" value="USC" className="filter" name="destination"/>
-                            <FormControlLabel control={<Checkbox checked={selectedValues.destination === 'San Diego'} onChange={handleQueryParamChange}/>} label="San Diego" value="San Diego" className="filter" name="destination"/>
-                            <FormControlLabel control={<Checkbox checked={selectedValues.destination === 'Koreatown'} onChange={handleQueryParamChange}/>} label="Koreatown" value="Koreatown" className="filter" name="destination"/>
-                            <FormControlLabel control={<Checkbox checked={selectedValues.destination === 'Irvine'} onChange={handleQueryParamChange}/>} label="Irvine" value="Irvine" className="filter" name="destination"/>
-                            <FormControlLabel control={<Checkbox checked={selectedValues.destination === 'Orange County'} onChange={handleQueryParamChange}/>} label="Orange County" value="Orange County" className="filter" name="destination"/>
+                        <span className="filter-category">Origin</span>
+                        <FormControlLabel control={<Checkbox checked={selectedValues.origin === 'UCLA'} onChange={handleQueryParamChange} />} label="UCLA" value="UCLA" className="filter" name="origin" />
+                        <FormControlLabel control={<Checkbox checked={selectedValues.origin === 'LAX'} onChange={handleQueryParamChange} />} label="LAX" value="LAX" className="filter" name="origin" />
+                        <FormControlLabel control={<Checkbox checked={selectedValues.origin === 'USC'} onChange={handleQueryParamChange} />} label="USC" value="USC" className="filter" name="origin" />
+                        <FormControlLabel control={<Checkbox checked={selectedValues.origin === 'San Diego'} onChange={handleQueryParamChange} />} label="San Diego" value="San Diego" className="filter" name="origin" />
+                        <FormControlLabel control={<Checkbox checked={selectedValues.origin === 'Koreatown'} onChange={handleQueryParamChange} />} label="Koreatown" value="Koreatown" className="filter" name="origin" />
+                        <FormControlLabel control={<Checkbox checked={selectedValues.origin === 'Irvine'} onChange={handleQueryParamChange} />} label="Irvine" value="Irvine" className="filter" name="origin" />
+                        <FormControlLabel control={<Checkbox checked={selectedValues.origin === 'Orange County'} onChange={handleQueryParamChange} />} label="Orange County" value="Orange County" className="filter" name="origin" />
+
+                        <span className="filter-category">Destination</span>
+                        <FormControlLabel control={<Checkbox checked={selectedValues.destination === 'UCLA'} onChange={handleQueryParamChange} />} label="UCLA" value="UCLA" className="filter" name="destination" />
+                        <FormControlLabel control={<Checkbox checked={selectedValues.destination === 'LAX'} onChange={handleQueryParamChange} />} label="LAX" value="LAX" className="filter" name="destination" />
+                        <FormControlLabel control={<Checkbox checked={selectedValues.destination === 'USC'} onChange={handleQueryParamChange} />} label="USC" value="USC" className="filter" name="destination" />
+                        <FormControlLabel control={<Checkbox checked={selectedValues.destination === 'San Diego'} onChange={handleQueryParamChange} />} label="San Diego" value="San Diego" className="filter" name="destination" />
+                        <FormControlLabel control={<Checkbox checked={selectedValues.destination === 'Koreatown'} onChange={handleQueryParamChange} />} label="Koreatown" value="Koreatown" className="filter" name="destination" />
+                        <FormControlLabel control={<Checkbox checked={selectedValues.destination === 'Irvine'} onChange={handleQueryParamChange} />} label="Irvine" value="Irvine" className="filter" name="destination" />
+                        <FormControlLabel control={<Checkbox checked={selectedValues.destination === 'Orange County'} onChange={handleQueryParamChange} />} label="Orange County" value="Orange County" className="filter" name="destination" />
                     </Container>
                     <Container className="rides-container">
                         {fetchedRides.map((item, index) => (
@@ -93,7 +84,7 @@ export default function RidesPage() {
                                 origin={item.origin}
                                 destination={item.destination}
                                 driver={item.driver}
-                                dateTime={item.dateTime}
+                                dateTime={item.displayDateTime}
                                 price={item.price}
                                 uid={item.uid}
                                 numSpots={item.numSpots}
