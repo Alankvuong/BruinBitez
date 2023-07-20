49f68440a6edc27c0c6e1cb3c9369577e3582803
Alan Vuong <alankvuong@gmail.com>
2023-07-20 04:51:55 -0700

    Feature: Added ability for users to sort rides by date or price + styling

--- bryft/src/pages/RidesPage/RidesPage.js
+++ bryft/src/pages/RidesPage/RidesPage.js
@@ -6,6 +6,23 @@ import React, { useState, useEffect } from 'react';
 import RideModal from "../../components/RideModal/RideModal";
 import RidePost from "../../components/RidePost/RidePost";
 import axios from "axios";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import MovingCar from "../../assets/cartoon-car.svg";
+import Select from '@mui/material/Select';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+
+const theme = createTheme({
+    typography: {
+        h6: {
+            fontSize: "20px"
+        },
+        fontFamily: [
+            'Amiko',
+        ].join(','),
+    },
+  });
 
 export default function RidesPage() {
     const [fetchedRides, setFetchedRides] = useState([]);
@@ -15,24 +32,45 @@ export default function RidesPage() {
         destination: '',
         dateTime: ''
     });
+    const [sortType, setSortType] = useState("date-descending");
+    const [sortedRides, setSortedRides] = useState();
+    const [isSortedByDateAscending, setIsSortedByDateAscending] = useState(true);
 
     useEffect(() => {
         console.log(queryParams);
         axios.get('http://localhost:8000/api/get-rides', { params: queryParams })
             .then((response) => {
+                setFetchedRides(response);
                 // Sort rides from closest date to farthest date
-                const sortRides = response.data.sort((a, b) => {
+                const sortedRides = response.data.sort((a, b) => {
                     const dateTimeA = new Date(a.dateTime);
                     const dateTimeB = new Date(b.dateTime);
                     return dateTimeA - dateTimeB;
                 });
 
-                setFetchedRides(sortRides);
+                if(!isSortedByDateAscending) {
+                    sortedRides.reverse();
+                }
+                
+                if(sortType === "date-ascending") {
+                    setSortedRides([...sortedRides].reverse());
+                    console.log("ascending");
+                } else if(sortType === "date-descending") {
+                    // ridesToSort.sort((a, b) => new Date(b.dateTime) - new Date(a.dateTime));
+                    setSortedRides([...sortedRides]);
+                    setIsSortedByDateAscending(false);
+                    console.log("descending");
+                    // console.log(ridesToSort);
+                } else if(sortType === "price-ascending") {
+                    setSortedRides([...sortedRides.sort((a, b) => parseFloat(a.price) - parseFloat(b.price))]);
+                } else if(sortType === "price-descending") {
+                    setSortedRides([...sortedRides.sort((a, b) => parseFloat(b.price) - parseFloat(a.price))]);
+                }                
             })
             .catch((error) => {
                 console.error('Error:', error);
             });
-    }, [queryParams]);
+    }, [sortType, isSortedByDateAscending, queryParams]);
 
     const handleQueryParamChange = (e) => {
 
@@ -47,19 +85,52 @@ export default function RidesPage() {
         }
     }
 
+    const handleSelectOptionsChange = (event) => {
+        const selectedSortType = event.target.value;
+        setSortType(selectedSortType);
+
+        console.log(sortedRides);
+    }
+
     return (
         <div>
             <Navbar />
+            <span className="rides-page-header">
+                <ThemeProvider theme={theme}>
+                    <Typography variant="h5" className="title-text">
+                        Where are you traveling?
+                    </Typography>
+                </ThemeProvider>
+                <img className="moving-car" src={MovingCar} alt="moving car" width="140" height="140" draggable="false"></img>
+            </span>
             <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" m={1} >
-                <Typography variant="h5" className="title-text">
-                    Where are you traveling to?
-                </Typography>
-                <Box display="flex" marginLeft="auto" mb={2} >
+                {/* Ride Post Button */}
+                <Box display="flex" marginLeft="auto" mb={2}>
                     <RideModal />
                 </Box>
-                <Box display="flex" flexDirection="row" width="100%">
-                    <Container className="filter-container">
 
+                <span className="sort-by-select">
+                    <FormControl className="sort-by-list" component="div">
+                        <InputLabel id="simple-select-label">Sort</InputLabel>
+                        <Select
+                            labelId="simple-select-label"
+                            id="simple-select"
+                            value={sortType}
+                            label="sort"
+                            onChange={handleSelectOptionsChange}
+                        >
+                            <MenuItem value="date-ascending">Date - Ascending</MenuItem>
+                            <MenuItem value="date-descending">Date - Descending</MenuItem>
+                            <MenuItem value="price-ascending">Price - Ascending</MenuItem>
+                            <MenuItem value="price-descending">Price - Descending</MenuItem>
+                        </Select>
+                    </FormControl>
+                </span>
+                <Box display="flex" flexDirection="row" width="100%" className="box-container">
+                    <Container className="filter-container">
+                        <ThemeProvider theme={theme}>
+                            <Typography variant="h6" className="filter-heading-text">Filter your destinations</Typography>
+                        </ThemeProvider>
                         <span className="filter-category">Origin</span>
                         <FormControlLabel control={<Checkbox checked={selectedValues.origin === 'UCLA'} onChange={handleQueryParamChange} />} label="UCLA" value="UCLA" className="filter" name="origin" />
                         <FormControlLabel control={<Checkbox checked={selectedValues.origin === 'LAX'} onChange={handleQueryParamChange} />} label="LAX" value="LAX" className="filter" name="origin" />
@@ -78,22 +149,20 @@ export default function RidesPage() {
                         <FormControlLabel control={<Checkbox checked={selectedValues.destination === 'Irvine'} onChange={handleQueryParamChange} />} label="Irvine" value="Irvine" className="filter" name="destination" />
                         <FormControlLabel control={<Checkbox checked={selectedValues.destination === 'Orange County'} onChange={handleQueryParamChange} />} label="Orange County" value="Orange County" className="filter" name="destination" />
                     </Container>
-                    <Container className="rides-container">
-                        {fetchedRides.map((item, index) => (
-                            <RidePost key={index}
-                                origin={item.origin}
-                                destination={item.destination}
-                                driver={item.driver}
-                                dateTime={item.displayDateTime}
-                                price={item.price}
-                                driverUid={item.driverUid}
-                                numSpots={item.numSpots}
-                                docId={item.docId}
-                                riderUids={item.riderUids}
+                    <Container className="rides-container" max-width="80%" width="100%">
+                        {sortedRides && sortedRides.map((ride, index) => (
+                            <RidePost
+                                ridesObject={ride}
+                                key={index}                            
                             />
                         ))}
                     </Container>
                 </Box>
+                <ThemeProvider theme={theme}>
+                    <Typography variant="h5" className="end-of-results-text">
+                    End of Results
+                    </Typography>
+                </ThemeProvider>
             </Box>
         </div>
     )
